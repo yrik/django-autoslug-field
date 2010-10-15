5ce40cdab070894a8e821d40c7c191cecbd18598
Aljosa Mohorovic <aljosa.mohorovic@gmail.com>
2010-10-15 16:26:54 +0200

    initial files

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-autoslug-field',
+    version = '0.1',
+    description = "AutoSlugField for Django based on django-extensions AutoSlugField, adds option to track parent field for slug.",
+    long_description = open('README.rst').read(),
+    author = 'Aljosa Mohorovic',
+    author_email = 'aljosa.mohorovic@gmail.com',
+    url='http://github.com/aljosa/django-autoslug-field/',
+    packages = find_packages(),
+    include_package_data = True,
+    license = "MIT License",
+    keywords = "django autoslug slug field",
+    platforms = ['any'],
+)
